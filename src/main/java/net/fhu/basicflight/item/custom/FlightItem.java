7ebcf3b242df66568de1dbae2f65ca3804da17b7
Damon <suppor1bounce@gmail.com>
2023-12-27 18:58:25 -0700

    Tried to get item fly working

--- src/main/java/net/fhu/basicflight/item/custom/FlightItem.java
+++ src/main/java/net/fhu/basicflight/item/custom/FlightItem.java
@@ -6,14 +6,24 @@ import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 
 public class FlightItem extends Item {
     public FlightItem(Properties pProperties) {
         super(pProperties);
     }
 
-
-
-
+    @Override
+    public void onInventoryTick(ItemStack stack, Level level, Player player, int slotIndex, int selectedIndex) {
+        super.onInventoryTick(stack, level, player, slotIndex, selectedIndex);
+        if(player.isLocalPlayer()) {
+            player.getAbilities().mayfly = true;
+            player.getAbilities().flying = true;
+        }else {
+            player.getAbilities().mayfly = false;
+            player.getAbilities().flying = false;
+        }
+    }
 }
