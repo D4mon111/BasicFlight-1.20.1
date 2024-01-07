71eee9f1569ae29befd138551d336a8e8980ff88
Console796 <geoffreyroberttaylor@gmail.com>
2024-01-06 16:23:20 -0800

    Removed unneeded `FlightItem` class

--- src/main/java/net/fhu/basicflight/item/ModItems.java
+++ src/main/java/net/fhu/basicflight/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.fhu.basicflight.item;
 
 import net.fhu.basicflight.BasicFlight;
-import net.fhu.basicflight.item.custom.FlightItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,7 +12,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, BasicFlight.MOD_ID);
 
     public static final RegistryObject<Item> RING = ITEMS.register("ring",
-            () -> new FlightItem(new Item.Properties()));
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
