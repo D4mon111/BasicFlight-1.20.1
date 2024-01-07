1b6455b175eae67229b6fd3733b80f3aeb9b8c8f
Damon <suppor1bounce@gmail.com>
2024-01-06 22:44:57 -0700

    cleaned Main java code

--- src/main/java/net/fhu/basicflight/BasicFlight.java
+++ src/main/java/net/fhu/basicflight/BasicFlight.java
@@ -3,28 +3,14 @@ package net.fhu.basicflight;
 import com.mojang.logging.LogUtils;
 import net.fhu.basicflight.registrys.CreativeTabRegistry;
 import net.fhu.basicflight.registrys.ItemRegistry;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.extensions.IForgeItem;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("basicflight")
 public class BasicFlight {
     public static final String MOD_ID = "basicflight";
@@ -38,32 +24,12 @@ public class BasicFlight {
 
         ItemRegistry.init();
         CreativeTabRegistry.init();
-
-
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ItemRegistry.RING);
-        }
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    private void commonSetup(final FMLCommonSetupEvent event) {}
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
     }
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
 
-        }
-    }
 }
